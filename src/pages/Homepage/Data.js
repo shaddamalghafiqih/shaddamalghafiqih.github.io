196015235162df3582b975087f3e1b8a914764ec
Shaddam Alghafiqih <shaddamalghafiqih11@gmail.com>
2020-12-03 21:08:48 +0700

    first commit

---
+++ src/pages/Homepage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Frontend Developer',
+  headline: "Hi, I'm Shaddam Alghafiqih Bazdha",
+  description:
+    'I Create a interface website using HTML, CSS, Javascript, React JS, Styled Component. ',
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../../images/code-review.svg'),
+  alt: 'Credit Card',
+  start: ''
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Instant Setup',
+  headline: 'Extremely quick onboarding process',
+  description:
+    "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../../images/svg-2.svg'),
+  alt: 'Vault',
+  start: ''
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Sarah Jeni',
+  headline:
+    'Ultra helped me increase my revenue by over 3X in less than 3 months!',
+  description:
+    "Their team is wonderful! I can't believe I didn't start working with them earlier.",
+  buttonLabel: 'View Case Study',
+  imgStart: 'start',
+  img: require('../../images/profile.jpg'),
+  alt: 'Vault',
+  start: 'true'
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Secure Database',
+  headline: 'All your data is stored on our secure server',
+  description:
+    'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
